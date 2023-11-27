13256da2739f47ec96d9cdc135c4c92902e98e8f
Nilesh-Parit <nileshparit4202@gmail.com>
2023-11-27 13:20:16 +0530

    27-11-2023

---
+++ Queue/src/TestQueue.java
@@ -0,0 +1,18 @@
+public class TestQueue {
+
+	public static void main(String[] args) {
+		MyQueue ob=new MyQueue(5);
+		ob.enqueue(25);
+		ob.enqueue(35);
+		ob.enqueue(45);
+		System.out.println(ob);
+		ob.enqueue(55);
+		ob.dequeue();
+		System.out.println(ob);
+		ob.dequeue();
+		ob.dequeue();
+		ob.dequeue();
+		System.out.println(ob);
+		ob.dequeue();
+	}
+}
